eae0256208621b6ed11ad6fc45c570fa7e7921f8
huyneeee <nqh.one.of.a.kind@gmail.com>
2022-01-09 19:26:04 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+require("dotenv").config();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [
+            process.env.ORIGIN1,
+            process.env.ORIGIN2,
+            process.env.ORIGIN3,
+            process.env.ORIGIN4,
+            process.env.ORIGIN5,
+        ],
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
